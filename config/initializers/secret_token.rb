cb2f7ff042fc857439761790d7de8ee4e91f942f
Matt Eves <mattheweves@outlook.com>
2015-08-03 00:56:00 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qbox::Application.config.secret_key_base = 'b140707d07878ba2f68c3ddca58104e9384b57c20cc6ad2190a9945086723b640bc9af5be2ea2f6085fe253d4100c8f72c5ba43fd701237fd4befc1c7629b66f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Qbox::Application.config.session_store :cookie_store, key: '_qbox_session'
